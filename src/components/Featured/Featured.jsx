d8dfe6b2760c62b20fd5e6e48e13624db3f50ef9
Amaranth-S02 <skamar003@gmail.com>
2025-01-10 22:33:34 +0530

    feature page changed and home

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import "./featured.scss"
+import React from 'react';
+import './featured.scss';
 
 const Featured = () => {
   return (
-    <div className='featured'>
-        <div className="container">
-             <div className="left">
-                <h1>
-                    Find the perfect <i>freelance</i> service for your business
-                </h1>
-                <div className="search">
-                    <div className="searchInput">
-                        <img src="./img/search.png" alt="" />
-                        <input type="text" placeholder='Try"Building Mobile App"'/>
-                    </div>
-                    <button>Search</button>
-                </div>
-                <div className="popular">
-                    <span>Popular:</span>
-                    <button>Ai Services</button>
-                    <button>Video & Animation</button>
-                    <button>Graphics & Design</button>
-                    <button>Logo Design</button>
-                    
-                    
-                </div>
-             </div>
-             <div className="right"></div>
-             <img src="./img/man.png" alt=""/>
-        </div>
-    </div>
-  )
-}
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          {/* Heading Section */}
+          <h1>
+            Connect with skilled <i>freelancers</i> <br />
+            to elevate your business
+          </h1>
+          
+          {/* Search Bar */}
+          <div className="search">
+            <div className="searchInput">
+              <img src="./img/search.png" alt="Search Icon" />
+              <input type="text" placeholder='Try "Building Mobile App"' />
+            </div>
+            <button>Search</button>
+          </div>
 
-export default Featured 
+          {/* Popular Section */}
+          <div className="popular">
+            <span>Popular:</span>
+            <button>AI Services</button>
+            <button>Video & Animation</button>
+            <button>Graphics & Design</button>
+            <button>Logo Design</button>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="right">
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,89 +5,90 @@ function Footer() {
   return (
     <div className="footer">
       <div className="container">
+        {/* Top Section */}
         <div className="top">
-          <div className="item">
-            <h2>Categories</h2>
-            <span>Graphics & Design</span>
-            <span>Digital Marketing</span>
-            <span>Writing & Translation</span>
-            <span>Video & Animation</span>
-            <span>Music & Audio</span>
-            <span>Programming & Tech</span>
-            <span>Data</span>
-            <span>Business</span>
-            <span>Lifestyle</span>
-            <span>Photography</span>
-            <span>Sitemap</span>
-          </div>
-          <div className="item">
+          {/* Contact Section */}
+
+            <div className="item">
             <h2>About</h2>
             <span>Press & News</span>
             <span>Partnerships</span>
             <span>Privacy Policy</span>
             <span>Terms of Service</span>
-            <span>Intellectual Property Claims</span>
-            <span>Investor Relations</span>
-            <span>Contact Sales</span>
           </div>
+          <div className="item">
+            <h2>Contact</h2>
+            <span>
+              <img src="/img/email.png" alt="Email Icon" /> skamar003@mail.com
+            </span>
+            <span>
+              <img src="/img/telephone.png" alt="Phone Icon" /> 9361136165
+            </span>
+            <span>
+              <img src="/img/location.png" alt="Location Icon" /> Coimbatore
+            </span>
+          </div>
+
+          {/* About Section */}
+        
+
+          {/* Support Section */}
           <div className="item">
             <h2>Support</h2>
             <span>Help & Support</span>
             <span>Trust & Safety</span>
-            <span>Selling on Liverr</span>
-            <span>Buying on Liverr</span>
+           
           </div>
+
+          {/* Community Section */}
           <div className="item">
             <h2>Community</h2>
-            <span>Customer Success Stories</span>
-            <span>Community hub</span>
+            <span>Community Hub</span>
             <span>Forum</span>
             <span>Events</span>
             <span>Blog</span>
-            <span>Influencers</span>
-            <span>Affiliates</span>
-            <span>Podcast</span>
-            <span>Invite a Friend</span>
-            <span>Become a Seller</span>
-            <span>Community Standards</span>
           </div>
+
+          {/* More from SkillBridge */}
           <div className="item">
             <h2>More From SkillBridge</h2>
             <span>SkillBridge Business</span>
             <span>SkillBridge Pro</span>
             <span>SkillBridge Logo Maker</span>
-            <span>SkillBridge Guides</span>
-            <span>Get Inspired</span>
-            <span>SkillBridge Select</span>
-            <span>ClearVoice</span>
-            <span>Workspace</span>
-            <span>Learn</span>
-            <span>Working Not Working</span>
+           
           </div>
         </div>
+
+        {/* Divider */}
         <hr />
+
+        {/* Bottom Section */}
         <div className="bottom">
+          {/* Left Section */}
           <div className="left">
-            <h2>SkillBridge</h2>
-            <span>©skillbrigde 2025</span>
+           
+            <span>©SkillBridge 2025</span>
           </div>
+
+          {/* Right Section */}
           <div className="right">
+            {/* Social Media */}
             <div className="social">
-              <img src="/img/twitter.png" alt="" />
-              <img src="/img/facebook.png" alt="" />
-              <img src="/img/linkedin.png" alt="" />
-              <img src="/img/pinterest.png" alt="" />
-              <img src="/img/instagram.png" alt="" />
+              <img src="/img/twitter.png" alt="Twitter Icon" />
+              <img src="/img/facebook.png" alt="Facebook Icon" />
+              <img src="/img/linkedin.png" alt="LinkedIn Icon" />
+              <img src="/img/pinterest.png" alt="Pinterest Icon" />
+              <img src="/img/instagram.png" alt="Instagram Icon" />
             </div>
+
+            {/* Currency */}
             <div className="link">
-              <img src="/img/language.png" alt="" />
-              <span>English</span>
-            </div>
-            <div className="link">
-              <img src="/img/rupeee.png" alt="" />
+              <img src="/img/rupeee.png" alt="Rupee Icon" />
               <span>INR</span>
             </div>
-            <img src="/img/accessibility.png" alt="" />
+
+            {/* Accessibility */}
+            <img src="/img/accessibility.png" alt="Accessibility Icon" />
           </div>
         </div>
       </div>
