38223b686f284f58b005043fddf6d7a638842c8c
ADMIN <skamar003@gmail.com>
2025-08-19 04:47:54 +0530

    cch

--- client/src/components/chatBot/Chatbot.jsx
+++ client/src/components/chatBot/Chatbot.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import './Chatbot.scss';
+import "./ChatBot.scss"
 
 const Chatbot = () => {
   const userData = JSON.parse(localStorage.getItem('currentUser'));
