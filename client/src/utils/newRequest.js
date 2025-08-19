5eac246c3626419f7e1161c44d35dcf3f892925c
ADMIN <skamar003@gmail.com>
2025-08-19 05:50:45 +0530

    cch

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: process.env.FRONTEND_URL || "http://localhost:3000/api/",
   withCredentials: true,
 });
 
 export default newRequest;
+
