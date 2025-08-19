2f97b6fa6b311b7cb1b139be56d0acb76bf60f53
ADMIN <skamar003@gmail.com>
2025-08-19 05:39:31 +0530

    cch

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-
 const newRequest = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://skillbridge-api.vercel.app/api/",
   withCredentials: true,
 });
-
-export default newRequest;
