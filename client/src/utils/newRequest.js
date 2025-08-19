649150e8c3d9d869767bc4c7dbd652bab4f84556
ADMIN <skamar003@gmail.com>
2025-08-19 10:55:26 +0530

    cch

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://skillbridge-backend-pmhk.onrender.com/api/"
+      : "http://localhost:3000/api/",
   withCredentials: true,
 });
 
